d7015747d06f928d119c587e651486bc88d5d867
Eduardo <130085121+dudeksx@users.noreply.github.com>
2023-09-27 10:30:21 -0300

    Updating "MyClass" and uploading a new archive

---
+++ src/BoletimEstudantil.java
@@ -0,0 +1,13 @@
+package edu.dudu.classAnatomy;
+public class BoletimEstudantil {
+
+  public static void main(String[] args) {
+    int finalAverage = 7;
+    if (finalAverage < 6) 
+        System.out.println("Failed"); 
+    else if (finalAverage == 6) 
+        System.out.println("Final Test"); 
+    else 
+        System.out.println("Approved");
+  }
+}
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,3 +1,4 @@
+package edu.dudu.classAnatomy;
 public class MyClass {
 
   public static void main(String[] args) {
